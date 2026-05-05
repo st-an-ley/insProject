06238b46fa9737db308446c98db5a6ed9c7528d7
Stanley <info@stanley-eitel.de>
2026-05-05 23:09:51 +0200

    update

--- src/secondApproach/CLIENT.py
+++ src/secondApproach/CLIENT.py
@@ -4,24 +4,49 @@ import streamlit as st
 #defining what every client has in common
 class Client:
     ID = 0 #Number of created Clients
-    def __init__(self, useCase, port, messagingType="SUB", protocol="tcp"):
+    def __init__(self, useCase, SUBport, PUBport, messagingType="SUB", protocol="tcp"):
         #Set the attributes to determine the type of client
         self.useCase = useCase
         self.messagingType = messagingType
         self.protocol = protocol
-        self.port = port
+        self.SUBport = SUBport
+        self.PUBport = PUBport
+        self.ID = ID
         print(f"Client for {self.useCase} with ID {self.ID} was created")
         ID = ID+1
 
-        #Create socket for each Client-object
+
         context = zmq.Context()
-        if self.messagingType == "SUB":
-            socket_sub = context.socket(zmq.SUB)
-  #TODO #else...    
-        socket_sub.connect(f"{self.protocol}://localhost:{self.port}")
+        
+        #Create SUBSCRIBER socket to receive data from the Server
+        socket_sub = context.socket(zmq.SUB)   
+        socket_sub.connect(f"{self.protocol}://localhost:{self.SUBport}")
         socket_sub.setsockopt(zmq.SUBSCRIBE, f"{self.useCase}.encode()") # encode() turns data into it's binary form
     
+        socket_sub = context.socket(zmq.PUB)   
+        socket_sub.bind(f"{self.protocol}://*:{self.PUBport}")
+
+
         while True:
             topic = socket_sub.recv_string()
             data = socket_sub.recv_pyobj()
 
+
+
+class checkVideoFeedCheating_client(Client):
+    def __init__(self, useCase, SUBport, PUBport, messagingType="SUB", protocol="tcp"):
+        Client.__init__(self, useCase, SUBport, PUBport, messagingType="SUB", protocol="tcp")
+
+    def checkVideoFeedCheating(videoInput):
+        print("Checking video for cheating. Need further implementation")
+
+
+
+class checkAudioFeedCheating_client(Client):
+    def __init__(self, useCase, SUBport, PUBport, messagingType="SUB", protocol="tcp"):
+        Client.__init__(self, useCase, SUBport, PUBport, messagingType="SUB", protocol="tcp")
+
+    def checkVideoFeedCheating(audioInput):
+        print("Checking audio for cheating. Need further implementation")
+
+#TODO add further client types
--- src/secondApproach/script.py
+++ src/secondApproach/script.py
@@ -1,10 +1,14 @@
 import CLIENT
-import StreamlitClient
+import start_streamlit
 import SERVER
 
+#main script to start the app
+#Create the server and clients and run them by using the package multiprocessing
+#Start streamlit in the background by using the package Subprocess to run a python script from within another python script
 
 def main():
-    pass
+    server = SERVER.Server()
+    
 
 if __name__ == "__main__":
     main()
