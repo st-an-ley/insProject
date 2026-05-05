b453bfc2f567374b568f1c54a5832b1fe9a47557
Stanley <info@stanley-eitel.de>
2026-05-05 20:34:06 +0200

    Added class Client

---
+++ CLIENT.py
@@ -0,0 +1,32 @@
+import zmq
+
+class Client:
+    ID = 0 #Number of creates Clients
+    def __init__(self, useCase, port, messagingType="SUB", protocol="tcp"):
+        #Set the attributes to determine the type of client
+        self.useCase = useCase
+        self.messagingType = messagingType
+        self.protocol = protocol
+        self.port = port
+        print(f"Client for {self.useCase} with ID {self.ID} was created")
+        ID = ID+1
+
+        #Create socket for each Client-object
+        context = zmq.Context()
+        if self.messagingType == "SUB":
+            socket_sub = context.socket(zmq.SUB)
+  #TODO #else...    
+        socket_sub.connect(f"{self.protocol}://localhost:{self.port}")
+        socket_sub.setsockopt(zmq.SUBSCRIBE, f"{self.useCase}.encode()") # encode() turns data into it's binary form
+        
+
+
+def main():
+    client_stream = Client("STREAM", "5555")
+    client_audio = Client("AUDIO", "5556")
+    client_video = Client("VIDEO", "5557")
+    client_sup = Client("SUP", "5558")
+    #...
+
+if __name__ == "__main__":
+    main()
